54f3cffc4a1d0a6f2eabe8580838d3e86bf7c537
noe <2020100612bp@psu.palawan.edu.ph>
2025-03-25 11:38:15 +0800

    add command to create initial data for colleges using Faker

--- try/core/management/commands/create_initial_data.py
+++ try/core/management/commands/create_initial_data.py
@@ -3,3 +3,23 @@ from django.db import transaction
 from faker import Faker
 from core.models import College, Program, Organization, Student, OrgMember
 import random
+
+class Command(BaseCommand):
+    help = 'Creates initial data with faker'
+
+    def handle(self, *args, **kwargs):
+        fake = Faker()
+        
+        try:
+            with transaction.atomic():
+                # Create Colleges
+                colleges = []
+                college_codes = ['CCIS', 'COE', 'COB', 'CAS', 'COEd', 'COM', 'COL', 'COA']
+                for i in range(8):
+                    college = College.objects.create(
+                        name=f"College of {fake.company()}",
+                        code=college_codes[i],
+                        description=fake.text()
+                    )
+                    colleges.append(college)
+                self.stdout.write(self.style.SUCCESS(f'Created {len(colleges)} colleges'))
